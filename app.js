e13e1e0b781dd893de9502c7a9f77f2f3193baa8
martinsetaro <lordsesony@gmail.com>
2022-08-14 23:02:03 -0300

    actualizacion del juego

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let container=document.querySelector('.container');
 let btn = document.querySelector('.btn');
 let text=document.querySelector('.entrada_texto');
+let incorrectas = document.querySelector('.incorrectas');
 
 
  
@@ -21,18 +22,37 @@ secretWord.forEach(item =>{
     
 })
 
-btn.addEventListener('click',buscar)
+btn.addEventListener('click',()=>{
+    buscar();
+    let letter = text.value;
+    text.value="";
+    let correct = false;
+    for(let i=0;i<secretWord.length;i++){
+        if(secretWord[i] == letter){
+            correct = true;
+            container.children[i].innerHTML=`<h2>${letter}</h2>`;
+           
+        }
+    }
+    if(!correct){
+         
+        incorrectas.innerHTML+=`<h2>${letter + " - "}</h2>`;
+    }
+
+   limpiarText();
+
+})
+   
+
 
 function buscar(){
-    
+  
     let b = document.querySelectorAll('.box_letter')
     b.forEach(item => {
         if(text.value == item.textContent){
             item.style.marginTop='0rem';
             discovery += item.textContent;
             
-        
-          
             if(discovery.length === palabra.length){
                 Swal.fire(
                     'GANASTES!',
@@ -46,15 +66,19 @@ function buscar(){
         }
         else 
         {
-            console.log("no esta")
+            
+           
         }
     })
-
+    
 }
- 
+
+
 function limpiarText(){
     text.value='';
 }
+
+
 
 
 
